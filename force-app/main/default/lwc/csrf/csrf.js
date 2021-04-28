4a505c7d8760fa5b8bef78e5afe5ed76ddb906a2
Alba Rivas <arivas@salesforce.com>
2021-04-28 10:04:31 +0200

    Add padding

---
+++ force-app/main/default/lwc/csrf/csrf.js
@@ -0,0 +1,23 @@
+import { LightningElement } from "lwc";
+
+export default class Csrf extends LightningElement {
+  connectedCallback() {
+    /*createAccount()
+      .then((data) => {
+        const toastEvent = new ShowToastEvent({
+          title: "Account created",
+          message: "Record ID: " + data.Id,
+          variant: "success"
+        });
+        this.dispatchEvent(toastEvent);
+      })
+      .catch((error) => {
+        const toastEvent = new ShowToastEvent({
+          title: "Error creating account",
+          message: "Error: " + reduceErrors(error).join(","),
+          variant: "error"
+        });
+        this.dispatchEvent(toastEvent);
+      });*/
+  }
+}
