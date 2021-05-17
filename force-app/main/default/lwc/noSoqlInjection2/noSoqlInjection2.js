a32cf9066bfabcbcaa49d1d1604903d0d37edce5
Alba Rivas <arivas@salesforce.com>
2021-05-17 13:12:54 +0200

    Flow example

---
+++ force-app/main/default/lwc/noSoqlInjection2/noSoqlInjection2.js
@@ -0,0 +1,32 @@
+import { LightningElement, wire } from "lwc";
+import { reduceErrors } from "c/ldsUtils";
+import NAME_FIELD from "@salesforce/schema/Account.Name";
+import REVENUE_FIELD from "@salesforce/schema/Account.AnnualRevenue";
+import INDUSTRY_FIELD from "@salesforce/schema/Account.Industry";
+import getFilteredAccounts from "@salesforce/apex/AccountController.getFilteredAccounts2";
+
+const COLUMNS = [
+  { label: "Account Name", fieldName: NAME_FIELD.fieldApiName, type: "text" },
+  {
+    label: "Annual Revenue",
+    fieldName: REVENUE_FIELD.fieldApiName,
+    type: "currency"
+  },
+  { label: "Industry", fieldName: INDUSTRY_FIELD.fieldApiName, type: "text" }
+];
+
+export default class NoSoqlInjection2 extends LightningElement {
+  columns = COLUMNS;
+  searchValue = "";
+
+  @wire(getFilteredAccounts, { searchValue: "$searchValue" })
+  accounts;
+
+  get errors() {
+    return this.accounts.error ? reduceErrors(this.accounts.error) : [];
+  }
+
+  handleInputChange(event) {
+    this.searchValue = event.target.value;
+  }
+}
